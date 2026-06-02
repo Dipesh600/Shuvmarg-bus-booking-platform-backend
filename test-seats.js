fe8de11e29f879185fb1e82e2d18e2de209fbe02
Dipesh chaudhary <dipeshch040313@gmail.com>
2026-06-03 00:55:51 +0530

    chore: remove test and script files

--- test-seats.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Seat = require('./models/seatsModel.js');
-
-mongoose.connect('mongodb://127.0.0.1:27017/shuvmarg').then(async () => {
-    const seat = await Seat.findOne({tripId: "6a0566848ebd7314d3e82abf"}).lean();
-    console.log(JSON.stringify(seat, null, 2));
-    mongoose.disconnect();
-});
