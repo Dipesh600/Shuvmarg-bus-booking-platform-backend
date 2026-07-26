abce6cfe96bed748bda51a69f28169bfbda50245
Dipesh <dipeshvh040313@gmail.com>
2026-07-26 13:48:51 +0530

    refactor(payment-booking): extract passenger wallet debit

---
+++ src/modules/booking/passenger-wallet-payment/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * src/modules/booking/passenger-wallet-payment/index.js
+ *
+ * Production composition root for passenger wallet payment debit module.
+ */
+
+const Wallet = require('../../../../models/walletModel');
+const smLedgerService = require('../../../../services/smLedgerService');
+const logger = require('../../../../utils/logger');
+const { createPassengerWalletPaymentRepository } = require('./passenger-wallet-payment.repository');
+const mapper = require('./passenger-wallet-payment.mapper');
+const { createPassengerWalletPaymentService } = require('./passenger-wallet-payment.service');
+
+const walletRepository = createPassengerWalletPaymentRepository({ Wallet });
+
+const passengerWalletPaymentService = createPassengerWalletPaymentService({
+  walletRepository,
+  smLedgerService,
+  logger,
+  mapper,
+});
+
+module.exports = {
+  debitPassengerWalletPayment: passengerWalletPaymentService.debitPassengerWalletPayment,
+};
